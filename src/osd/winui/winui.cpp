f1f574ebd1753f6b77b50f2a59ad6132775dac79
Robbbert <Robbbert@users.noreply.github.com>
2022-10-20 23:23:40 +1100

    ***Get rid of the mutex crap which was locking up the system if an error was made at the command line.

--- src/osd/winui/winui.cpp
+++ src/osd/winui/winui.cpp
@@ -644,7 +644,9 @@ int MameUIMain(HINSTANCE hInstance, LPWSTR lpCmdLine)
 	if (__argc != 1)
 	{
 		extern int main_(int argc, char *argv[]);
-		exit(main_(__argc, __argv));
+		int result = main_(__argc, __argv);
+		printf("exit result = %d\n",result);
+		exit(result);
 	}
 
 	// No printf's allowed before here, otherwise they get into queries from mame, such as listxml.
