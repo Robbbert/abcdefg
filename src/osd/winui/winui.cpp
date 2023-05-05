a42c5b5ba8f6276443d9877db5b1b3d98940362f
Robbbert <Robbbert@users.noreply.github.com>
2023-05-06 02:13:53 +1000

    revert a bit of yesterday's change

--- src/osd/winui/winui.cpp
+++ src/osd/winui/winui.cpp
@@ -499,7 +499,7 @@ static void load_translation(emu_options &m_options)
 	util::load_translation(file);
 }
 
-static std::string s_output_buffer;
+//static std::string s_output_buffer;
 
 class mameui_output_error : public osd_output
 {
@@ -537,14 +537,14 @@ public:
 
 		if (s_action)
 		{
-//			// if we are in fullscreen mode, go to windowed mode
-//			if ((video_config.windowed == 0) && !osd_common_t::window_list().empty())
-//				winwindow_toggle_full_screen();
+			// if we are in fullscreen mode, go to windowed mode
+			if ((video_config.windowed == 0) && !osd_common_t::window_list().empty())
+				winwindow_toggle_full_screen();
 
-//			winui_message_box_utf8(!osd_common_t::window_list().empty() ?
-//				dynamic_cast<win_window_info &>(*osd_common_t::window_list().front()).platform_window() :
-//					hMain, buffer, MAMEUINAME, (BIT(s_action, 0) ? MB_ICONINFORMATION : MB_ICONERROR) | MB_OK);
-			s_output_buffer.append(buffer);
+			winui_message_box_utf8(!osd_common_t::window_list().empty() ?
+				dynamic_cast<win_window_info &>(*osd_common_t::window_list().front()).platform_window() :
+					hMain, buffer, MAMEUINAME, (BIT(s_action, 0) ? MB_ICONINFORMATION : MB_ICONERROR) | MB_OK);
+//			s_output_buffer.append(buffer);
 		}
 
 //		else
@@ -633,13 +633,13 @@ static void RunMAME(int nGameIndex, const play_options *playopts)
 	// Increment our playtime
 	IncrementPlayTime(nGameIndex, elapsedtime);
 
-	if (!s_output_buffer.empty())
-	{
-		winui_message_box_utf8(!osd_common_t::window_list().empty() ?
-			dynamic_cast<win_window_info &>(*osd_common_t::window_list().front()).platform_window() :
-				hMain, s_output_buffer.c_str(), MAMEUINAME, MB_ICONINFORMATION | MB_OK);
-		s_output_buffer.clear();
-	}
+//	if (!s_output_buffer.empty())
+//	{
+//		winui_message_box_utf8(!osd_common_t::window_list().empty() ?
+//			dynamic_cast<win_window_info &>(*osd_common_t::window_list().front()).platform_window() :
+//				hMain, s_output_buffer.c_str(), MAMEUINAME, MB_ICONINFORMATION | MB_OK);
+//		s_output_buffer.clear();
+//	}
 
 	// the emulation is complete; continue
 	for (int i = 0; i < std::size(s_nPickers); i++)
@@ -650,7 +650,7 @@ int MameUIMain(HINSTANCE hInstance, LPWSTR lpCmdLine)
 {
 	// delete old log file, ignore any error
 	unlink("winui.log");
-	unlink("verbose.log");
+//	unlink("verbose.log");
 
 	if (__argc != 1)
 	{
