43642ae03aeca7c74e52ddcaa63e654db9ef341c
Robbbert <Robbbert@users.noreply.github.com>
2023-09-29 13:54:54 +1000

    Get rid of test code, bump release version.

--- src/osd/winui/winui.h
+++ src/osd/winui/winui.h
@@ -85,7 +85,7 @@
 #endif
 #define MAMENAME			"MAME"
 
-#define MAME_VERSION		"0.259.0"
+#define MAME_VERSION		"0.259.1"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
