ae2bdaf7d645d68ae558f64a307ec6bd635d145e
Robbbert <Robbbert@users.noreply.github.com>
2023-10-25 18:44:01 +1100

    Files for 260

--- docs/release/build/make.bat
+++ docs/release/build/make.bat
@@ -8,4 +8,7 @@ copy /Y src\mame\arcade.txt src\mame\arcade.flt
 touch src\mame\arcade.flt
 rem call mk.bat
 call make64 -j4 "OSD=winui" %1 %2 %3
+if not exist arcade64.exe goto end
+del a.exe
 copy /Y arcade64.exe a.exe
+:end
