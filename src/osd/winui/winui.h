0e8c547ebee9e684e089ac10e6d65f9d118f2ff7
Robbbert <Robbbert@users.noreply.github.com>
2024-11-27 11:03:36 +1100

    Files for 272.

--- src/osd/winui/winui.h
+++ src/osd/winui/winui.h
@@ -85,7 +85,7 @@
 #endif
 #define MAMENAME			"MAME"
 
-#define MAME_VERSION		"0.271.x"
+#define MAME_VERSION		"0.272.0"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
