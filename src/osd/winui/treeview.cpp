4ce9863ed9759760029a4eb29a437b4eec2d2c2a
Robbbert <Robbbert@users.noreply.github.com>
2026-06-02 16:02:11 +1000

    If cache file missing, recreate it.

--- src/osd/winui/treeview.cpp
+++ src/osd/winui/treeview.cpp
@@ -41,7 +41,7 @@ static void FreeExtraFolders(void);
 static void SetExtraIcons(char *name, int *id);
 static bool TryAddExtraFolderAndChildren(int parent_index);
 static bool TrySaveExtraFolder(LPTREEFOLDER lpFolder);
-static void LoadExternalFolders(int parent_index, int id);
+static bool LoadExternalFolders(int parent_index, int id);
 static void SaveExternalFolders(int parent_index);
 static bool FilterAvailable(int driver_index);
 
@@ -942,83 +942,95 @@ void CreateDumpingFoldersIni(int parent_index)
 
 static void CreateCPUFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FC_CPU);
+
+	if (!res)
 	{
 		CreateCPUFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FC_CPU);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 20, 0);
 }
 
 static void CreateSoundFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FC_SOUND);
+
+	if (!res)
 	{
 		CreateSoundFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FC_SOUND);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 95, 0);
 }
 
 static void CreateScreenFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FC_MONITOR);
+
+	if (!res)
 	{
 		CreateScreenFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FC_MONITOR);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 80, 0);
 }
 
 static void CreateResolutionFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FC_MONITOR);
+
+	if (!res)
 	{
 		CreateResolutionFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FC_MONITOR);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 65, 0);
 }
 
 static void CreateFPSFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FP_FPS);
+
+	if (!res)
 	{
 		CreateFPSFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FP_FPS);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 50, 0);
 }
 
 static void CreateDumpingFolders(int parent_index)
 {
-	if (RequiredDriverCache())
+	bool res = false;
+	if (!RequiredDriverCache())
+		res = LoadExternalFolders(parent_index, IDI_FP_DUMP);
+
+	if (!res)
 	{
 		CreateDumpingFoldersIni(parent_index);
 		SaveExternalFolders(parent_index);
 	}
-	else
-		LoadExternalFolders(parent_index, IDI_FP_DUMP);
 
 	SendMessage(GetProgressBar(), PBM_SETPOS, 35, 0);
 }
 
-static void LoadExternalFolders(int parent_index, int id)
+static bool LoadExternalFolders(int parent_index, int id)
 {
 	const char* fname = NULL;
 	LPTREEFOLDER lpFolder = treeFolders[parent_index];
@@ -1028,14 +1040,14 @@ static void LoadExternalFolders(int parent_index, int id)
 			fname = g_lpFolderData[j].short_name;
 
 	if (fname == NULL)
-		return;
+		return false;
 
 	char filename[MAX_PATH];
 	snprintf(filename, std::size(filename), "%s\\%s", GetGuiDir(), fname);
 	FILE *f = fopen(filename, "r");
 
 	if (f == NULL)
-		return;
+		return false;
 
 	char readbuf[256];
 	char *name = NULL;
@@ -1093,6 +1105,7 @@ static void LoadExternalFolders(int parent_index, int id)
 	}
 
 	fclose(f);
+	return true;
 }
 
 static void SaveExternalFolders(int parent_index)
